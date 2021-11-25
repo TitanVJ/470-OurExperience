01c7a0e64a756429d764544123f88e2705642125
VJ <vbakhshi@sfu.ca>
2021-11-24 18:29:55 -0800

    updated app server and middleware files to use teh routes i added

--- project-backend/src/middlewares.ts
+++ project-backend/src/middlewares.ts
@@ -1,21 +1,24 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
+import createError from 'http-errors';
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-  const error = new Error(`Not found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
+  next( createError(404, `Not found - ${req.originalUrl}`) );
 };
 
 const errorHandler: ErrorRequestHandler = (err, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode).json({
-    status: statusCode,
-    message: err.message,
-    stack: err.stack
-  });
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 };
 
 export default {
   notFound,
   errorHandler
 };
+
+// THIS SEPERATES THE MIDDLEWARE FOR HANNDLING ERRORS
+// can be part of app.js(this case server.js) however
