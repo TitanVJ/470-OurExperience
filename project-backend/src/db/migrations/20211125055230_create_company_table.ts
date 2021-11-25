4ace7407b2f5e271eedf90f5aa00f43f6ffa7d44
Keegan McGinnis <kmcginni@sfu.ca>
2021-11-24 22:06:04 -0800

    Add migration to create company table

---
+++ project-backend/src/db/migrations/20211125055230_create_company_table.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('Company', (table: Knex.CreateTableBuilder) => {
+    table.increments();
+    table.string('name', 100);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('Company');
+}
