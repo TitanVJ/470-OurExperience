30ee529d6b347e8390dbdb5b0f99c65a58215a85
VJ <vbakhshi@sfu.ca>
2021-11-26 16:53:04 -0800

    added new routes for the job postings page and added csp config to allow some cdns

--- our-experience-app/src/server.ts
+++ our-experience-app/src/server.ts
@@ -8,6 +8,7 @@ import middlewares from './middlewares';
 const indexRouter = require('./routes/index');
 const studentRouter = require('./routes/student');
 const companyRouter = require('./routes/company');
+const careerRouter = require('./routes/career');
 
 const app = express();
 
@@ -16,7 +17,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(morgan('tiny'));
-app.use(helmet());
+
+const scriptSources = ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'cdn.datatables.net', 'cdnjs.cloudflare.com'];
+const styleSources = ["'self'", "'unsafe-inline'", 'cdn.datatables.net'];
+const connectSources = ["'self'"];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+        scriptSrc: scriptSources,
+        scriptSrcElem: scriptSources,
+        styleSrc: styleSources,
+        connectSrc: connectSources
+    },
+  })
+);
+
+app.use( helmet({ contentSecurityPolicy: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -26,7 +44,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Routes
 app.use('/', indexRouter);
 app.use('/student', studentRouter);
-app.use('/company', companyRouter);
+// app.use('/company', companyRouter);
+app.use('/career', careerRouter);
 
 // catch 404's and handle erros
 app.use(middlewares.notFound);
