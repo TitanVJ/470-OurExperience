18338539c04de219be58b420952d54079a1d4271
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-05 02:12:14 -0800

    8: Add role to user table

--- our-experience-app/src/db/migrations/20211123051022_initial.ts
+++ our-experience-app/src/db/migrations/20211123051022_initial.ts
@@ -2,10 +2,11 @@ import { Knex } from 'knex';
 import tableNames from '../../constants/tableNames';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable(tableNames.user, (table) => {
+  await knex.schema.createTable(tableNames.user, (table: Knex.CreateTableBuilder) => {
     table.increments().notNullable();
     table.string('username').notNullable();
     table.string('password', 127).notNullable();
+    table.enum('role', ['Student', 'Staff']);
   });
 }
 
