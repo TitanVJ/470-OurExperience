6105d85a52c0520cb3b8068f5c6bec9e18977fa2
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-07 00:59:10 -0800

    8: Restructure seeds into single file so we have control over order they run in

--- our-experience-app/src/db/seeds/project_seed.ts
+++ our-experience-app/src/db/seeds/project_seed.ts
@@ -1,10 +1,28 @@
 import { Knex } from 'knex';
+import bcrypt from 'bcryptjs';
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
+  await knex.raw('SET FOREIGN_KEY_CHECKS=0;');
+  await knex('ApplicationDocument').truncate();
+  await knex('Document').truncate();
+  await knex('JobApplication').truncate();
   await knex('JobPosting').truncate();
+  await knex('Company').truncate();
+  await knex('User').truncate();
+  await knex.raw('SET FOREIGN_KEY_CHECKS=1;');
 
   // Inserts seed entries
+  const password = 'password';
+  const salt = await bcrypt.genSalt(12);
+  const user = {
+    username: 'test',
+    password: await bcrypt.hash(password, salt)
+  };
+  await knex('User').insert(user);
+
+  await knex('Company').insert([{ name: 'A Thinking Ape' }, { name: 'EA Games' }, { name: 'Microsoft' }]);
+
   await knex('JobPosting').insert([
     {
       companyId: 1,
