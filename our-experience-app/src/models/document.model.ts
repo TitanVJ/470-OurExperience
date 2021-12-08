885a2bf275579471dc39c66f1a002494916a5778
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-07 20:34:12 -0800

    8: Add document user relation

--- our-experience-app/src/models/document.model.ts
+++ our-experience-app/src/models/document.model.ts
@@ -4,4 +4,17 @@ export class Document extends Model {
   static get tableName() {
     return 'Document';
   }
+
+  static get relationMappings() {
+    return {
+      user: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: `${__dirname}/user.model`,
+        join: {
+          from: 'Document.userId',
+          to: 'User.id'
+        }
+      }
+    };
+  }
 }
