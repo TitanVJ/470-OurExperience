652c6f57a6b5cc7b6fef10f33ede0c48e40919c5
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-08 17:59:03 -0800

    5: Add posting relation to application model, display applications on view

--- our-experience-app/src/models/job_application.model.ts
+++ our-experience-app/src/models/job_application.model.ts
@@ -4,4 +4,17 @@ export class JobApplication extends Model {
   static get tableName() {
     return 'JobApplication';
   }
+
+  static get relationMappings() {
+    return {
+      posting: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: `${__dirname}/job_posting.model`,
+        join: {
+          from: 'JobApplication.postingId',
+          to: 'JobPosting.id'
+        }
+      }
+    };
+  }
 }
--- our-experience-app/src/routes/application.ts
+++ our-experience-app/src/routes/application.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { JobApplication } from '../models/job_application.model';
 
 const router = express.Router();
 
 router.get('', async (req: Request, res: Response, next: NextFunction) => {
-  res.render('application_list', { title: 'My Job Applications', applications: [] });
+  const applications = await JobApplication.query().withGraphFetched('posting');
+  console.log('APPS:', applications);
+  res.render('application_list', { title: 'My Job Applications', applications: applications });
 });
 
 export = router;
