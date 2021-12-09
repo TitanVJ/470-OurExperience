9da9f34bdd6541a60a628857e06bdb98c2b5293c
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-08 23:52:39 -0800

    5: Add luxon and add nicer format to dates

--- our-experience-app/src/routes/application.ts
+++ our-experience-app/src/routes/application.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { JobApplication } from '../models/job_application.model';
+const { DateTime } = require('luxon');
 
 const router = express.Router();
 
 router.get('', async (req: Request, res: Response, next: NextFunction) => {
   const applications = await JobApplication.query().withGraphJoined('posting.[company]');
   console.log('APPS:', applications);
-  res.render('application_list', { title: 'My Job Applications', applications: applications });
+  res.render('application_list', { title: 'My Job Applications', applications: applications, DateTime: DateTime });
 });
 
 export = router;
