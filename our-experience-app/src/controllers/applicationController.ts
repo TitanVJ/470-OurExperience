d364a8867e1c62b000b4262a8d50a67da4f70cb5
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-10 20:58:54 -0800

    5: Change controller to query applications by user id (hard-coded for now)

--- our-experience-app/src/controllers/applicationController.ts
+++ our-experience-app/src/controllers/applicationController.ts
@@ -3,7 +3,7 @@ import { JobApplication } from '../models/job_application.model';
 const { DateTime } = require('luxon');
 
 const getApplicationListByUserId = async (req: Request, res: Response, next: NextFunction) => {
-  const applications = await JobApplication.query().withGraphJoined('posting.[company]');
+  const applications = await JobApplication.query().findByIds([1]).withGraphJoined('posting.[company]');
   res.render('application_list', { title: 'My Job Applications', applications: applications, DateTime: DateTime });
 };
 
