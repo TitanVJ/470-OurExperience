5a975401fbdadcc6b22cba4667648b8d06144151
Alden Ilao <ailao@sfu.ca>
2021-12-10 22:48:28 -0800

    created route and ui for applying to job

--- our-experience-app/src/routes/career.ts
+++ our-experience-app/src/routes/career.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { Company } from '../models/company.model';
 import { JobPosting } from '../models/job_posting.model';
+import { Document } from '../models/document.model';
 
 const router = express.Router();
 
@@ -23,7 +24,25 @@ router.get('/job/:id', async (req: Request, res: Response, next: NextFunction) =
 
 router.get('/job/:id/apply', async (req: Request, res: Response, next: NextFunction) => {
   const job: any = await JobPosting.query().findById(req.params.id).withGraphFetched('company');
-  res.render('application', { title: `${job.title} - ${job.company.name}` });
+  const userDocuments: Document[] = await Document.query().where('userId', req.user.id);
+  const formTypes: any = {
+    resume: [],
+    coverLetter: [],
+    transcript: [],
+    sis: []
+  };
+  userDocuments.forEach((document: Document) => {
+    if (document.documentType === 'resume') {
+      formTypes.resume.push(document);
+    } else if (document.documentType === 'cover-letter') {
+      formTypes.coverLetter.push(document);
+    } else if (document.documentType === 'transcript') {
+      formTypes.transcript.push(document);
+    } else if (document.documentType === 'sis') {
+      formTypes.sis.push(document);
+    }
+  });
+  res.render('application', { title: `${job.title} - ${job.company.name}`, job: job, formTypes: formTypes });
 });
 
 export = router;
