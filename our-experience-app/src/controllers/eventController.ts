9c97fa77234c427533910a4139521cd6a398d322
Keegan McGinnis <kmcginni@sfu.ca>
2021-12-12 13:51:44 -0800

    13: Add view for list of events

--- our-experience-app/src/controllers/eventController.ts
+++ our-experience-app/src/controllers/eventController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 const getEventList = (req: Request, res: Response, next: NextFunction) => {
-  res.render('index', { title: 'Events' });
+  res.render('event_list', { title: 'Events', events: [{ title: 'Test Event', location: 'Test Location', date: 'Dec 13, 2021, 1:25 AM' }] });
 };
 
 const getCalendarByUserId = (req: Request, res: Response, next: NextFunction) => {};
