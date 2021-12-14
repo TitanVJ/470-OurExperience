80c3e928c9dfd20337a451ed2878f033319656d4
VJ <vbakhshi@sfu.ca>
2021-12-13 16:58:02 -0800

    nicer admin dashboard with some *stats*

--- our-experience-app/src/controllers/admin.controller.ts
+++ our-experience-app/src/controllers/admin.controller.ts
@@ -1,7 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
+import { Company } from '../models/company.model';
+import { JobPosting } from '../models/job_posting.model';
 
-const dashboard = (req: Request, res: Response, next: NextFunction) => {
-    res.render('admin/dashboard', {title: 'Administrator Dashboard'});
+const dashboard = async (req: Request, res: Response, next: NextFunction) => {
+    const company_c: any = await Company.query().select(
+        Company.query().count().as('count')
+    );
+    const job_c:any = await JobPosting.query().select(
+        JobPosting.query().count().as('count')
+    );
+
+    const payload = { c_count: company_c[0].count, j_count: job_c[0].count };
+    res.render('admin/dashboard', {title: 'Administrator Dashboard', stats: payload });
 };
 
 export default { dashboard }
