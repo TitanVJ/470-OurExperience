8aa3af68432b54f0e656c33b3cc4998ff7d724dc
Alden Ilao <ailao@sfu.ca>
2021-12-13 18:03:19 -0800

    removed unused user route

--- our-experience-app/src/routes/index.ts
+++ our-experience-app/src/routes/index.ts
@@ -1,8 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-import userRoute from './users.routes';
 const router = express.Router();
-
-router.use('/user', userRoute);
 
 /* GET home page. */
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
